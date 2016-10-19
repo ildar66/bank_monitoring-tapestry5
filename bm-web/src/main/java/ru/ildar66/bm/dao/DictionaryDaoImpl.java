509cde44d61e4eaa7150190a6b9b213eb112b43a
ildar shafigullin <ildar66@inbox.ru>
2016-10-19 18:57:13 +0300

    add spring configuration files for production mode. add skeleton implementation for DAO Dictionaries.

---
+++ bm-web/src/main/java/ru/ildar66/bm/dao/DictionaryDaoImpl.java
@@ -0,0 +1,25 @@
+package ru.ildar66.bm.dao;
+
+import java.util.List;
+
+import ru.ildar66.bm.common.entity.Client;
+
+/**
+ * DAO implementation for Dictionaries
+ * 
+ * @author Shafigullin Ildar
+ * 
+ */
+public class DictionaryDaoImpl implements DictionaryDao {
+	
+	public List<Client> getClients(int startIndex, int i, String clientNamePattern) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getClientCount(String clientNamePattern) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
